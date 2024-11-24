ff6814864683e528cc43ed2d977cd19a71d7652d
mansoor1123456 <164507941+mansoor1123456@users.noreply.github.com>
2024-11-24 21:34:33 +0500

    first commit

--- page.tsx
+++ page.tsx
@@ -1,17 +1,18 @@
-import Image from "next/image";
 import Link from "next/link";
-import Navbar from "./navbar/page";
-import HeroPage from "./home/page";
-import Contact from "./contact/page";
-import About from "./about/page";
 
-// Thank you for taking the time to visit my website!
-
-export default function Home() {
-  return (
-    <>
-    < Navbar />
-    <HeroPage />
-    </>
-  );
+function ThankyouPage(){
+    return(
+        <>
+        <center>
+        <div className="bg-blue-900 h-screen">
+            <h1 className="pt-32 text-white font-bold text-7xl">Thank you for shopping!!!</h1>
+            <p className="text-white pt-3  line-through">Aty jaty rahiyega</p>
+            <p className="text-white text-xl pt-7 pb-8 font-semibold">Your order will be delivered in 3 Business days</p>
+            <Link href="/"><button className="bg-white text-black px-4 py-2 rounded-lg text-lg">Go Back</button></Link>
+        </div>
+        </center>
+        </>
+    )
 }
+
+export default ThankyouPage;
